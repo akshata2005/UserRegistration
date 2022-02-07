638ee106c953789b69185d01374ec09c995a23be
akshata2005 <akshatasawant1805@gmail.com>
2022-02-07 18:17:48 +0530

    [Akshata] Add: Validate Password with at least one upper case

--- UserRegistration/UserRegistration/RegexValidation.cs
+++ UserRegistration/UserRegistration/RegexValidation.cs
@@ -78,7 +78,8 @@ namespace UserRegistration
         //method for validating password
         public static void Password()
         {
-            string regex = "^[a-zA-Z]{8,}$";
+            // string regex = "^[a-zA-Z]{8,}$";
+            string regex = "^[A-Z]{1}[a-z]{8,}$";
             Console.WriteLine("Enter your Password :");
             string password = Console.ReadLine();
             Regex rg = new Regex(regex);
