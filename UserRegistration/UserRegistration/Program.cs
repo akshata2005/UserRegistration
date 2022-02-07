8198a0ae45d28a6e86c84da60c6d584a4fef75f9
akshata2005 <akshatasawant1805@gmail.com>
2022-02-07 10:59:25 +0530

    [Akshata] Add: Validate FirstName

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidation regexValidation = new RegexValidation();//Create object
+            Console.WriteLine("Enter Your First Name::-");//take input from the user
+            string FirstName = Console.ReadLine();//Store the input for firstname
+            Console.WriteLine(regexValidation.ValidateFirstName(FirstName));
+        }
+    }
+}
